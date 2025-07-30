4825498427877fc08f4759c2ad0427e0763dee47
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-07-30 22:18:32 +0300

    Implement REST API on frontend

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -1,4 +1,4 @@
-const BASE_URL = '/api/bug'
+const BASE_URL = '/api/bug/'
 
 export const bugService = {
     query,
@@ -8,38 +8,24 @@ export const bugService = {
     getDefaultFilter
 }
 
-function query(filterBy) {
-    return axios.get(BASE_URL)
+function query(filterBy = {}) {
+    return axios.get(BASE_URL, { params: filterBy })
         .then(res => res.data)
-        .then(bugs => {
-
-            if (filterBy.txt) {
-                const regExp = new RegExp(filterBy.txt, 'i')
-                bugs = bugs.filter(bug => regExp.test(bug.title))
-            }
-
-            if (filterBy.minSeverity) {
-                bugs = bugs.filter(bug => bug.severity >= filterBy.minSeverity)
-            }
-
-            return bugs
-        })
 }
 
 function getById(bugId) {
-    return axios.get(BASE_URL + '/' + bugId)
+    return axios.get(BASE_URL + bugId)
         .then(res => res.data)
 }
 
 function remove(bugId) {
-    return axios.get(BASE_URL + '/' + bugId + '/remove')
+    return axios.delete(BASE_URL + bugId)
         .then(res => res.data)
 }
 
 function save(bug) {
-    const queryStr = `/save?title=${bug.title}&description=${bug.description}&severity=${bug.severity}&_id=${bug.id || ''}`
-    return axios.get(BASE_URL + queryStr)
-        .then(res => res.data)
+    if (bug._id) return axios.put(BASE_URL + bug._id, bug).then(res => res.data)
+    else return axios.post(BASE_URL, bug).then(res => res.data)
 }
 
 
